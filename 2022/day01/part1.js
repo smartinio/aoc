f58c1e08bd6a39b499c67ed0ca4eebb4955c703c
Sam Martin <me@smartin.io>
2022-12-01 22:22:23 +0100

    2022 day1

---
+++ 2022/day01/part1.js
@@ -0,0 +1,10 @@
+const sum = (acc, x) => acc + x
+
+const result = require('./input.txt')
+  .split('\n\n')
+  .map((x) => x.split('\n').map(Number).reduce(sum, 0))
+  .sort((a, b) => b - a)
+  .slice(0, 1)
+  .reduce(sum, 0)
+
+console.log({ result })

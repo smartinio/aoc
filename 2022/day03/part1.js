821dd5464bafaa45b3e17a1750601d75a4a5b666
Sam Martin <me@smartin.io>
2022-12-04 00:09:02 +0100

    2022 day03

---
+++ 2022/day03/part1.js
@@ -0,0 +1,21 @@
+const input = require('./input.txt')
+
+const priority = Object.fromEntries(
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    .split('')
+    .map((val, i) => [val, i + 1])
+)
+
+const result = input
+  .trim()
+  .split('\n')
+  .map((items) => [
+    items.slice(0, items.length / 2).split(''),
+    items.slice(items.length / 2, items.length).split(''),
+  ])
+  .map(([left, right]) => [left, new Set(right)])
+  .map(([list, set]) => list.find((x) => set.has(x)))
+  .map((letter) => priority[letter])
+  .reduce((a, b) => a + b, 0)
+
+console.log({ result })
---
+++ 2022/day03/part2.js
@@ -0,0 +1,22 @@
+const input = require('./input.txt')
+
+const priority = Object.fromEntries(
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    .split('')
+    .map((val, i) => [val, i + 1])
+)
+
+const result = input
+  .trim()
+  .split('\n')
+  .flatMap((_, i, list) => {
+    if (i % 3 !== 0) return []
+    return [[list[i], list[i + 1], list[i + 2]]]
+  })
+  .map((data) => data.map((x) => x.split('')))
+  .map(([a, b, c]) => [a, new Set(b), new Set(c)])
+  .map(([list, setA, setB]) => list.find((x) => setA.has(x) && setB.has(x)))
+  .map((letter) => priority[letter])
+  .reduce((a, b) => a + b, 0)
+
+console.log({ result })

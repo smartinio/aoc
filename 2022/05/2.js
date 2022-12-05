c17fba5d68c0e063a3bb43bcde5e178662f29f60
Sam Martin <me@smartin.io>
2022-12-06 00:46:37 +0100

    2022 day 5

---
+++ 2022/05/2.js
@@ -0,0 +1,46 @@
+const [rawCrateData, rawMoves] = require('./input.txt')
+  .trimEnd()
+  .split(/\n\n/)
+  .map((part) => part.split('\n'))
+
+const crates = rawCrateData.slice(0, -1)
+
+const cratePositions = rawCrateData.slice(-1)[0]
+
+const crateNumberByIndex = {}
+{
+  const numberRegex = /\d/g
+  let crateNumbers
+
+  while ((crateNumbers = numberRegex.exec(cratePositions)) !== null) {
+    crateNumberByIndex[numberRegex.lastIndex] = crateNumbers[0]
+  }
+}
+
+const stacks = crates.reverse().reduce((acc, crate) => {
+  const crateRegex = /([A-Z])/g
+  let crateIds
+
+  while ((crateIds = crateRegex.exec(crate)) !== null) {
+    const crateNumber = crateNumberByIndex[crateRegex.lastIndex]
+    acc[crateNumber] = acc[crateNumber] || []
+    acc[crateNumber].push(crateIds[0])
+  }
+
+  return acc
+}, {})
+
+for (const rawMove of rawMoves) {
+  const amount = Number(rawMove.match(/\d+/)[0])
+  const [from, to] = rawMove.split(' from ')[1].split(' to ')
+
+  const crates = stacks[from].splice(-amount, amount)
+  stacks[to].push(...crates)
+}
+
+const result = Object.values(stacks).reduce(
+  (message, stack) => message + stack[stack.length - 1],
+  ''
+)
+
+console.log({ result })

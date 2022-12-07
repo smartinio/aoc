1f66f3927b30ddbdc9affd3388a55e0323706cce
Sam Martin <me@smartin.io>
2022-12-07 20:51:13 +0100

    2022 day 7

---
+++ 2022/07/1.js
@@ -0,0 +1,32 @@
+const MAX_SIZE = 100000
+const lines = require('./input.txt').trim().split('\n')
+
+let result = 0
+
+let finalDir = lines.reduce((current, line) => {
+  if (line === '$ cd ..') {
+    if (current.size <= MAX_SIZE) result += current.size
+    current.parent.size += current.size
+    return current.parent
+  }
+
+  if (line.startsWith('$ cd')) {
+    return { parent: current, size: 0 }
+  }
+
+  if (/^\d+/.test(line)) {
+    const [size] = line.split(' ')
+    current.size += Number(size)
+    return current
+  }
+
+  return current
+}, null)
+
+while (finalDir.parent) {
+  if (finalDir.size <= MAX_SIZE) result += finalDir.size
+  finalDir.parent.size += finalDir.size
+  finalDir = finalDir.parent
+}
+
+console.log({ result })
---
+++ 2022/07/2.js
@@ -0,0 +1,38 @@
+const CAPACITY = 70000000
+const NEEDED_SPACE = 30000000
+
+const lines = require('./input.txt').trim().split('\n')
+const dirs = []
+
+let finalDir = lines.reduce((current, line) => {
+  if (line === '$ cd ..') {
+    dirs.push(current)
+    current.parent.size += current.size
+    return current.parent
+  }
+
+  if (line.startsWith('$ cd')) {
+    return { parent: current, size: 0 }
+  }
+
+  if (/^\d+/.test(line)) {
+    const [size] = line.split(' ')
+    current.size += Number(size)
+    return current
+  }
+
+  return current
+}, null)
+
+while (finalDir.parent) {
+  finalDir.parent.size += finalDir.size
+  finalDir = finalDir.parent
+}
+
+const availableSpace = CAPACITY - finalDir.size
+const minSpaceToReclaim = NEEDED_SPACE - availableSpace
+const result = dirs
+  .sort((a, b) => a.size - b.size)
+  .find((dir) => dir.size >= minSpaceToReclaim).size
+
+console.log({ result })

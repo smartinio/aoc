377d5974ac34c9d9eeee9ad3c48391b16faa3106
Sam Martin <me@smartin.io>
2022-12-11 20:17:27 +0100

    2022 day 11

---
+++ 2022/11/1.js
@@ -0,0 +1,50 @@
+const monkeys = require('./input.txt')
+  .split('\n\n')
+  .map((monkey) => monkey.split('\n').slice(1))
+  .map((attrs) => attrs.map((attr) => attr.split(': ')[1]))
+  .map(([startingItems, operation, test, ifTrue, ifFalse], i) => ({
+    id: i,
+    items: startingItems.split(', ').map((n) => ({ worry: Number(n) })),
+    operation: operation.split('new = old ')[1].split(' '),
+    modTest: Number(test.split('divisible by ')[1]),
+    modTestTrueMonkey: Number(ifTrue.split('throw to monkey ')[1]),
+    modTestFalseMonkey: Number(ifFalse.split('throw to monkey ')[1]),
+  }))
+
+const inspects = Object.fromEntries(monkeys.map(({ id }) => [id, 0]))
+
+const inspect = (monkey) => {
+  if (!monkey.items.length) return
+
+  const { items, operation, modTest, modTestTrueMonkey, modTestFalseMonkey } =
+    monkey
+
+  const [operator, val] = operation
+
+  for (const item of items) {
+    const factor = val === 'old' ? item.worry : Number(val)
+
+    if (operator === '+') item.worry += factor
+    if (operator === '*') item.worry *= factor
+
+    item.worry = Math.floor(item.worry / 3)
+
+    const modTestTrue = item.worry % modTest === 0
+    const throwMonkeyId = modTestTrue ? modTestTrueMonkey : modTestFalseMonkey
+
+    monkeys[throwMonkeyId].items.push(item)
+    inspects[monkey.id]++
+  }
+
+  monkey.items = []
+}
+
+for (let i = 0; i < 20; i++) {
+  for (const monkey of monkeys) {
+    inspect(monkey)
+  }
+}
+
+const [monkeyA, monkeyB] = Object.values(inspects).sort((a, b) => b - a)
+
+log({ result: monkeyA * monkeyB })

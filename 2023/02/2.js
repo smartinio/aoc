01380de3f02260c6abc3e97fcd9d61f06119340a
Sam Martin <me@smartin.io>
2023-12-03 03:22:14 +0100

    Edgy golfing

--- 2023/02/2.js
+++ 2023/02/2.js
@@ -1,26 +1,25 @@
+const colors = ['red', 'green', 'blue']
+const regex = new RegExp(`\\d+ (${colors.join('|')})`, 'g')
+
 const result = require('./input.txt')
   .split('\n')
   .map((line) => line.split(';'))
   .map((sets, i) => ({
     id: i + 1,
     sets: sets.map((s) =>
-      s.match(/\d+ (red|green|blue)/g).reduce(
-        (acc, sample) => {
-          const [count, color] = sample.split(' ')
-          acc[color] = Number(count)
-          return acc
-        },
-        { red: 0, green: 0, blue: 0 }
-      )
+      s.match(regex).reduce((acc, sample) => {
+        const [count, color] = sample.split(' ')
+        acc[color] = Number(count)
+        return acc
+      }, Object.fromEntries(colors.map((c) => [c, 0])))
     ),
   }))
-  .map((game) => {
-    const red = Math.max(...game.sets.map((s) => s.red))
-    const green = Math.max(...game.sets.map((s) => s.green))
-    const blue = Math.max(...game.sets.map((s) => s.blue))
-
-    return red * green * blue
-  })
+  .map((game) =>
+    colors.reduce(
+      (power, color) => power * Math.max(...game.sets.map((set) => set[color])),
+      1
+    )
+  )
   .reduce((acc, power) => acc + power, 0)
 
 log({ result })
